274d880e5a691742ee01c7516c4db8855f3e589a
Sebastien Noret <sebastien.noret@gmail.com>
2023-04-05 12:59:07 +0200

    delete some old code

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// ReactDOM.createRoot(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
